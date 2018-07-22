53ee3a7f04dcd8ea235ab93c19c36718590adacb
Nochtal <nochtal@gmail.com>
2018-07-21 18:13:36 -0700

    Solution Added
    Solution contains Auditor, Auditor.Test, and AuditorDLL projects.

---
+++ AuditorSolution/Auditor.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Auditor.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
