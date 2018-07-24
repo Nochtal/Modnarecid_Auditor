6f104cea48d7010061c0bb0c0636dec330ad0bfe
Nochtal <nochtal@gmail.com>
2018-07-24 09:19:57 -0700

    Async Tasks GET/POST
    Added async static Task<HttpContent> for GetRequest(uri) and PostRequest(uri, content).

--- AuditorSolution/AuditorDLL/Goblin.cs
+++ AuditorSolution/AuditorDLL/Goblin.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace AuditorDLL
@@ -10,14 +8,51 @@ namespace AuditorDLL
     {
         public VexAdmin VA = new VexAdmin();
         private string APIROOTPATH { get; set; } = @"https://www.bungie.net/Platform";
-        private string APIKEY { get; set; } = ""; // Not shared
+        private static string KEY { get; set; } = "X-API-KEY";
+        private static string APIKEY { get; set; } = ""; // Not shared, must provide own for use of code. Otherwise see published app for functionality.
         public DataLattice DL = new DataLattice();
 
-        private bool SetUser()
+        /// <summary>
+        /// Async Task to return Http GET API call
+        /// </summary>
+        /// <param name="uri">API URI</param>
+        /// <returns>HttpContent returned by API</returns>
+        public async static Task<HttpContent> GetRequestTask(string uri)
         {
-            string APICommand = APIROOTPATH + DL.GetBungieNetUserById(VA.BUNGIEID);
-
-            return false;
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(KEY, APIKEY);
+                using (HttpResponseMessage response = await client.GetAsync(uri))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        return content;
+                    }
+                }
+            }
         }
+
+        /// <summary>
+        /// Async Task to perform an Http POST API call
+        /// </summary>
+        /// <param name="uri">API URI</param>
+        /// <param name="SC">HttpContent containing parameters, null if none</param>
+        /// <returns>HttpContent returned by API</returns>
+        public async static Task<HttpContent> PostRequest(string uri, HttpContent SC)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(KEY, APIKEY);
+                using (HttpResponseMessage response = await client.PostAsync(uri, SC))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        return content;
+                    }
+                }
+            }
+        }
+
+
     }
 }
